dbdbc848b1e3956a87ff374aa7a041758a8537af
Anand Jebakumar <anandjsamuel@gmail.com>
2020-10-28 09:19:15 -0700

    some more cleaning up

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class Menu extends Component{
-    
+
     constructor(props){
         super(props);
     }
